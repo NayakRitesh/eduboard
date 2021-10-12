716b2c62606dfe381ff6db560a42857ee943c1f6
NayakRitesh <ritesh.nayak@soarlogic.com>
2021-10-12 19:35:57 +0530

    added requirements

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from api import views
+
+app_name = 'api'
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.form, name='form')
 ]
